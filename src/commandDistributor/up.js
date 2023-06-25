b6e3a94a666b34e6e187810111db119add0608f2
Syderi <syderi@yahoo.com>
2023-06-25 18:47:22 +0300

    fix: up

--- src/commandDistributor/up.js
+++ src/commandDistributor/up.js
@@ -1,10 +1,18 @@
 import { currentPath } from '../const/currentPath.js';
 import { SYSTEM_SLASH } from '../const/systemSlash.js';
+import { platform } from 'node:process';
 
 export function up() {
   const parts = currentPath.path.split(SYSTEM_SLASH);
+  if (platform !== "win32") {parts.shift()};
+
   if (parts.length > 1) {
     parts.pop();
     currentPath.path = parts.join(SYSTEM_SLASH);
+  } else {
+    if (platform === "win32") return;
+    if (platform !== "win32") {
+      currentPath.path = SYSTEM_SLASH;
+    }
   }
 }
